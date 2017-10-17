42068edf8c571241a2e33a1d2cc8be24c9fda1aa
Jonathan Arouca <jonathanarouca@gmail.com>
2017-10-17 14:43:15 -0400

    install webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: {
+    path: './src/main.js'
+  },
+  output: {
+    path: __dirname+'/build',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './build'
+  }
+};
