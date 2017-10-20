83f4a0abd9ab0aa08a97e7774a3d79f465b54c80
Jonathan Arouca <jonathanarouca@gmail.com>
2017-10-19 23:02:27 -0400

    edit main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require('./app.scss');
+
 var textContent = require('./content');
 
 function appendListItem() {
@@ -11,3 +13,10 @@ function appendListItem() {
 
 var button = document.getElementById("add_button");
 button.addEventListener("click", appendListItem);
+
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(function() {
+    button.removeEventListener("click", appendListItem);
+  });
+}
